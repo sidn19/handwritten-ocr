19dd9ad9fec2db17f2e2984e9dbb48bce88291f5
Arc313 <nairadisi18ce@student.mes.ac.in>
2021-02-28 18:35:57 +0530

    Image capture

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from bottle import get, run, view
+from bottle import get, post, run, view, request, BaseRequest, TEMPLATES
+from PIL import Image
+from io import BytesIO
+import pytesseract
+import base64
+
+BaseRequest.MEMFILE_MAX = 10 * 1024 * 1024 # 10 MB
 
 @get('/')
 @view('index')
 def index():
     pass
 
-run(host='localhost', port=8080, debug=True, reloader=True)
+@post('/')
+def get_text():
+    data = request.json['image'].replace('data:image/png;base64,', '')
+    data = base64.b64decode(data)
+    return pytesseract.image_to_string(Image.open(BytesIO(data)))
+
+run()
